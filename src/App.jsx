07318914ec36f5c3b60bf138bd5ade6d79498d58
Yonatan Ariel <yonatanriel@gmail.com>
2025-07-16 13:19:36 +0300

    refactor: remove unnecessary context wrapper from Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import './App.css';
 import Layout from './components/layout/Layout';
 
 function App() {
-  //need to run: npm run setup to install all new versions of the dependencies
-  //no need to run npm install
   return <Layout />;
 }
 
