e65b03f2634fb27e2561b93afd611082a5540c82
Yonatan Ariel <yonatanriel@gmail.com>
2025-07-16 20:44:31 +0300

    feat: update routing to include ProtectedRoute for all main components

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,23 +3,76 @@ import AdminDashboard from '@/components/layout/AdminDashboard';
 import AiMatching from '@/pages/AiMatching';
 import MemberProfile from '@/pages/MemberProfile';
 import ConnectionsRequests from '@/pages/ConnectionsRequests';
-import UserProfile from '@/pages/UserProfile'; // Add this import
-import { Routes, Route } from 'react-router-dom';
+import UserProfile from '@/pages/UserProfile';
 import Login from '@/pages/Login';
+import { Routes, Route } from 'react-router-dom';
 import Excel from '@/components/ui/Excel';
+import LinkedinLogin from '@/pages/LinkedinLogin';
+import ProtectedRoute from '@/components/ui/ProtectedRoute';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<AdminDashboard />} />
-      <Route path="/ai-matching" element={<AiMatching />} />
-      <Route path="/connections-requests" element={<ConnectionsRequests />} />
-      <Route path="/userprofile" element={<UserProfile />} />
-      <Route path="/member/:id" element={<MemberProfile />} />
-      <Route path="/excel" element={<Excel />} />
+      <Route path="/linkedin-login" element={<LinkedinLogin />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/ai-matching"
+        element={
+          <ProtectedRoute>
+            <AiMatching />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/connections"
+        element={
+          <ProtectedRoute>
+            <ConnectionsRequests />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/userprofile"
+        element={
+          <ProtectedRoute>
+            <UserProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/member/:id"
+        element={
+          <ProtectedRoute>
+            <MemberProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/excel"
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <Excel />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
+
 export default AppRoutes;
